9dd6e27d8880107ce4be7189c8677ed44bea56d9
nuhuxi <mark.keckeis@gmail.com>
2014-05-12 15:54:34 -0700

    first commit

---
+++ googlemapapi.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	$('#getGPS').click( function(event){
+
+        street = $('#streetAddress').val()+'+';
+        city = $('#city').val()+'+';
+        state = $('#state').val();
+
+        // the parameters we need to pass in our request to Google Geocode's API
+        googleURL1 = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+        googleURL2 = '&sensor=true&key=AIzaSyBJh8OQe2-Ni97hUnshrLde2CnkYG4TQTQ';
+        
+        var request =
+            googleURL1+
+            street+
+            city+
+            state+
+            googleURL2;
+
+        var google_response = $.ajax({
+            url: request,
+            type: "GET"}).done(function (data) {
+                
+                console.log('Latitude is: ' + data.results[0].geometry.location.lat);
+                console.log('Longitude is is: ' + data.results[0].geometry.location.lng);
+            
+                $('#latitude').setAttribute('text',data.results[0].geometry.location.lat);
+                $('#logitude').setAttribute('text',data.results[0].geometry.location.lng);
+
+            });
+
+
+
+    });
+    
+    $('#clearForm').click( function(event){
+        $("#streetAddress").val("");
+        $("#city").val("");
+        $("#state").val("");
+    });
+    
+
+    $('form').submit(function(e){e.preventDefault();});
+            
+});
+
+
+
+
+
+
+
+
+
+
+
